ceb8085fe6c6f52e24417eb339f927d81abe4788
Michael Coates <popcorndevils@gmail.com>
2022-09-17 13:02:06 -0400

    sprite has reel

--- src/aspects/common/ianimate.cs
+++ src/aspects/common/ianimate.cs
@@ -1,4 +1,3 @@
-
 namespace Rogui
 {
     public enum AnimateDirection
--- src/aspects/flicker/flicker.cs
+++ src/aspects/flicker/flicker.cs
@@ -1,3 +1,5 @@
+using System.Collections.Generic;
+using SFML.Graphics;
 
 namespace Rogui
 {
@@ -7,5 +9,33 @@ namespace Rogui
 
         public AnimateState State { get; set; }
         public float AnimSpeed { get; set; }
+        public Dictionary<string, Reel> Animations;
+
+        public string? Animation {
+            get => this._Animation;
+            set {
+                if(value is not null && this.Animations.ContainsKey(value))
+                {
+                    this._Animation = value;
+                }
+            }
+        }
+
+        public Flicker()
+        {
+            this.Animations = new Dictionary<string, Reel>();
+        }
+
+        public void AddReel(string name, Reel reel)
+        {
+            this.Animations.Add(name, reel);
+        }
+
+        public void AddReel(string name, params Sprite[] sprites)
+        {
+            this.Animations.Add(name, new Reel(sprites));
+        }
+
+        private string? _Animation;
     }
 }
