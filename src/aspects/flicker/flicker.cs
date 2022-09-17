a401479b22e971c65d6f2a8ca74791eba644039e
Michael Coates <popcorndevils@gmail.com>
2022-09-17 00:21:09 -0400

    noting bug with buttons after close

--- src/aspects/flicker/flicker.cs
+++ src/aspects/flicker/flicker.cs
@@ -6,7 +6,6 @@ namespace Rogui
         public event EventHandler<AnimateState>? AnimationFinished;
 
         public AnimateState State { get; set; }
-        public AnimateDirection AnimDirection { get; set; }
         public float AnimSpeed { get; set; }
     }
 }
